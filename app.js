33bae514c7f0d966e52572f7bd514c12f56ace72
abe-lst92 <abraham.sauz7@gmail.com>
2020-12-01 21:02:50 -0700

    this is my first

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+
+// alert("My name is Abraham S");
+// confirm("Are you at least 18?");
+// prompt("Are you hungry?");
+
+
+//this will get the time\\
+
+// var name = "abraham"; //string
+// var age = 20; //int-num
+// var movie = "fast and furious";//string
+// var isExercising = true; //bolean
+// var dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+// console.log(name + ' ' + age + ' ' + movie + ' ' + ' ' + isExercising)
+// console.log(dayOfWeek [2]);
+
+var currentTime = new Date();
+console.log(currentTime) 
+
+
+var time = document.getElementById("time");
+time.innerHTML = currentTime.toLocaleTimeString();
+
+
+
+//this will get the date\\
+
+var date= document.getElementById("date");
+// date.innerHTML = currentTime.toLocalDateString();
+date.innerHTML = currentTime.toLocaleDateString();
+
+
+// var date = document.getElementById("date");
+// date.innerHTML = currentTime.toLocaleDateString();
+
+
+
+//this will get the day of the week\\
+var dayWeeks = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var wekeD = document.getElementById("weekD");
+wekeD.innerHTML = dayWeeks[currentTime.getDay()];
