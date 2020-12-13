41f04a9b7a7fcacc60a6734d0ad5ee53443ba773
abe-lst92 <abraham.sauz7@gmail.com>
2020-12-13 13:35:04 -0700

    nooooooooooo

--- app.js
+++ app.js
@@ -27,6 +27,23 @@ chritsmas();
 },1000)
 
 
+$.fn.christmas = function() {
+    $(this).each(function() {
+      $(this).html($(this).text().split("").map(function(v, i) {
+        return '<span class="christmas-' + (i % 2 == 0 ? 'gold' : 'blue') + '">' + v + '</span>';
+      }).join(""));
+    });
+  };
+  
+  $(function() { 
+    $('h1.christmas').christmas();
+  });
+
+
+
+
+
+
 
 
 
